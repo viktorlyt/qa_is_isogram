bf57a4dd6d5ddbb67a550149f1fed1b1bce85049
viktorlyt <viktorlyt@gmail.com>
2023-05-12 17:27:54 +0300

    done

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,16 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`true if empty string`, () => {
+    expect(isIsogram('')).toBe(true);
+  });
+
+  it(`true if no repeating letters in the string`, () => {
+    expect(isIsogram('playgrounds')).toBe(true);
+  });
+
+  it(`false if has repeating letters`, () => {
+    expect(isIsogram('look')).toBe(false);
+  });
 });
